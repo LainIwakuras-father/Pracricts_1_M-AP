273d93bc63d9827fef8f5a5f80809f53cba8f3e8
Ury Dol3wsky <dol3wsky@gmail.com>
2025-09-09 22:25:57 +0300

    ПРАКТИАКА ПО МИАП НОМЕР 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SyncDemo;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(400, 225);
+        this.Text = "Form1";
+
+
+        // Инициализация ProgressBar
+        this.progressBar1 = new ProgressBar();
+        this.progressBar1.Location = new System.Drawing.Point(12, 12);
+        this.progressBar1.Size = new System.Drawing.Size(300, 23);
+        this.progressBar1.Minimum = 0;
+        this.progressBar1.Maximum = 100;
+        this.progressBar1.Value = 0;
+
+            // Инициализация кнопки Plus
+        this.buttonPlus = new Button();
+        this.buttonPlus.Location = new System.Drawing.Point(12, 50);
+        this.buttonPlus.Size = new System.Drawing.Size(75, 23);
+        this.buttonPlus.Text = "Plus";
+        this.buttonPlus.Click += new System.EventHandler(this.ButtonPlus_Click);
+
+            // Инициализация кнопки Minus
+        this.buttonMinus = new Button();
+        this.buttonMinus.Location = new System.Drawing.Point(100, 50);
+        this.buttonMinus.Size = new System.Drawing.Size(75, 23);
+        this.buttonMinus.Text = "Minus";
+        this.buttonMinus.Click += new System.EventHandler(this.ButtonMinus_Click);
+
+            // Инициализация таймера
+        this.timer1 = new System.Windows.Forms.Timer();
+        this.timer1.Interval = 1000;
+        this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+
+            // Настройка формы
+        this.Controls.Add(this.progressBar1);
+        this.Controls.Add(this.buttonPlus);
+        this.Controls.Add(this.buttonMinus);
+        this.FormClosing += new FormClosingEventHandler(this.Form1_FormClosing);
+    }
+
+    #endregion
+}
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SyncDemo;
+
+public partial class Form1 : Form
+{
+
+
+    Thread PlusThread = null;
+    public static object mtxA = new object();
+    public static object mtxB = new object();
+    public delegate void Progress();
+    public Progress progressDelegate;
+    public Progress minusDelegate;
+
+
+    // Элементы которые должны быть объявлены
+    private ProgressBar progressBar1;
+    private Button buttonPlus;
+    private Button buttonMinus;
+    private System.Windows.Forms.Timer timer1;
+    public Form1()
+    {
+        InitializeComponent();
+        progressDelegate = new Progress(StepMethod);
+        minusDelegate = new Progress(MinusMethod);
+    }
+
+    private void ThreadMinusFunc()
+    {
+        while (true)
+        {
+            lock (mtxA)
+            {
+                lock (mtxB)
+                {
+                    try
+                    {
+                        Invoke(minusDelegate);
+
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e.Message);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    private void ThreadPlusFunc()
+    {
+        while (true)
+        {
+            lock (mtxA)
+            {
+                lock (mtxB)
+                {   
+                    try
+                    {
+                        Invoke(progressDelegate);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e.Message);
+                        return;
+                    }
+                    
+                }
+            }
+        }
+    }
+
+
+
+    public void StepMethod()
+    {
+        if (progressBar1.Value < progressBar1.Maximum)
+                progressBar1.Value += 1;
+    }
+    public void MinusMethod()
+    {
+        if (progressBar1.Value > progressBar1.Minimum)
+            progressBar1.Value -= 1;
+    }
+    private void ButtonPlus_Click(object sender, EventArgs e)
+    {
+        if (PlusThread == null)
+        {
+            PlusThread = new Thread(new ThreadStart(ThreadPlusFunc))
+            {
+                IsBackground = true,
+                Priority = ThreadPriority.Normal
+            };
+            PlusThread.Start();
+            timer1.Enabled = false;
+            buttonPlus.Enabled = false;
+        }
+    }
+    private void ButtonMinus_Click(object sender, EventArgs e)
+    {
+        Thread MinusThread = new Thread(new ThreadStart(ThreadMinusFunc))
+        {
+            IsBackground = true,
+            Priority = ThreadPriority.Normal
+        };
+        MinusThread.Start();
+        timer1.Enabled = true;
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        if (PlusThread != null)
+            PlusThread.Abort();
+    }
+    private void Timer1_Tick(object sender, EventArgs e)
+    {
+        Text = DateTime.Now.ToLongTimeString();
+    }
+
+
+}
